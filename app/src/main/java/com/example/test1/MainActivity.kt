3ebe684e64b2a4d18a29c165e027b7fdd215af24
Stavroula <papoutsistavroula@gmail.com>
2024-10-23 18:35:26 +0300

    onboarding commit

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 //////lalala
+///////lalal after onboarding
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
